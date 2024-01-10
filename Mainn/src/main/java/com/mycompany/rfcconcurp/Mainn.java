8ce226933a4d05cf40d944aff1864425fd329e27
sayo-melocojon <145472048+sayo-melocojon@users.noreply.github.com>
2024-01-10 01:02:28 -0600

    Add files via upload

---
+++ Mainn/src/main/java/com/mycompany/rfcconcurp/Mainn.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.rfcconcurp;
+
+/**
+ *
+ * @author Alberto Rivero
+ */
+public class Mainn {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
